b9fb3a6d787e0c28f1dde9eddf5fb7bcbb689b2d
Ntaganira kwitonda theos <kwitondatheos@gmail.com>
2026-09-23 12:19:08 +0200

    bug fix

--- src/config/db.js
+++ src/config/db.js
@@ -4,8 +4,12 @@ import 'dotenv/config'
 
 // connection pools automatically use existing connections intead of openning new ones
 
-const pool = mysql.createPool({
-    url: process.env.DATABASE_URL,
+const pool = mysql.createPool(process.env.DATABASE_URL || {
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10, // maximum simultaneous connections
     queueLimit: 0,
